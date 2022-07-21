8892a492db8b3933ac33aff5841b95f9f30f63c1
Pauline <paulineleturque8@gmail.com>
2022-07-21 17:43:47 +0200

    init & test

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="purple darken-4" dark>
+      <div class="d-flex align-center pa-11">CSV-APP</div>
+    </v-app-bar>
+    <v-container class="pa-11">
+      <Test />
+    </v-container>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+import Test from "./components/Test.vue";
+export default {
+  name: "App",
+  components: { Test },
+};
+</script>
---
+++ src/components/Test.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container class="pa-10">
+    <v-spacer></v-spacer>
+    <div>
+      <v-file-input accept=".csv" label="Choisir un fichier"> </v-file-input>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Test",
+};
+</script>
